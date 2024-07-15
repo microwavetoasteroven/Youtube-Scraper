c9be0d077e822a2234f2577d698e6c27d7d1dec1
Quality Driven Software Ltd <zachary.gander@prophetfrom.ai>
2024-07-15 22:12:49 +0100

    setup

--- setup_fastapi.sh
+++ setup_fastapi.sh
@@ -70,6 +70,11 @@ cat << EOF > README.md
 # $PROJECT_NAME
 This is a minimal FastAPI project setup.
 
+## Setting up venv
+To create your python virtual environment and install dependencies, use: 
+
+./create_venv.sh
+
 ## Running the Application
 To run the application, from root directoy my_fastapi_app, use:
 
