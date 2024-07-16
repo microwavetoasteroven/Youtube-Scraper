c212adde1decdcb126b372038b6a7eb9db715355
Quality Driven Software Ltd <zachary.gander@prophetfrom.ai>
2024-07-16 08:16:46 +0100

    search added

--- my_fastapi_app/app/main.py
+++ my_fastapi_app/app/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 from app.config import config
 import os
+from app.util.youtube_api import search_videos_by_keyword
+import logging
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    print(config.SOMETHING)
-    print(os.environ['YOUTUBE_API'])
     return {"message": "Hello World"}
+
+
+@app.get("/youtube_search/{keyword}/{max_pages}/{results_per_page}")
+async def youtube_search(keyword:str, max_pages: int, results_per_page:int):
+    # Example of how to use this generator
+    result = []
+    for video in search_videos_by_keyword(keyword, max_results=results_per_page, max_pages=max_pages):
+        print(f"Video ID: {video['video_id']}, Title: {video['title']}")
+        result.append(video)
+
+    return {"message": result}
--- my_fastapi_app/app/util/youtube_api.py
+++ my_fastapi_app/app/util/youtube_api.py
@@ -1,26 +1,40 @@
 from googleapiclient.discovery import build
 from app.config import config
-
-
+from functools import lru_cache
 
 def yt():
     api_key = config.YOUTUBE_API
     return build('youtube', 'v3', developerKey=api_key)
 
-def search_videos_by_keyword(keyword, max_results=10):
+@lru_cache(maxsize=20)
+def search_videos_by_keyword(keyword, max_results=10, max_pages=5):
     youtube = yt()
-    request = youtube.search().list(
-        part="snippet",
-        q=keyword,
-        maxResults=max_results,
-        type="video"
-    )
-    response = request.execute()
+    next_token = None
+    page_count = 0
 
-    for item in response['items']:
-        video_id = item['id']['videoId']
-        title = item['snippet']['title']
-        print(f"Video ID: {video_id}, Title: {title}")
+    while page_count < max_pages:
+        request = youtube.search().list(
+            part="snippet",
+            q=keyword,
+            maxResults=max_results,
+            pageToken=next_token,
+            type="video"
+        )
+        response = request.execute()
 
-# Example usage
-search_videos_by_keyword("FastAPI")
+        for item in response['items']:
+            video_id = item['id']['videoId']
+            title = item['snippet']['title']
+            # Instead of printing, yield each video as a dictionary
+            yield {'video_id': video_id, 'title': title}
+
+        next_token = response.get('nextPageToken')
+        if not next_token:
+            break  # Exit the loop if there's no more pages
+
+        page_count += 1  # Increment the page count
+
+
+
+
+
