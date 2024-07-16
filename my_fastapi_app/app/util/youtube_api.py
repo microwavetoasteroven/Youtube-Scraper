787a34a3eea5c9e822322a019c450ee4d7a9f134
Quality Driven Software Ltd <zachary.gander@prophetfrom.ai>
2024-07-16 20:58:44 +0100

    youtube updated to use json dumps:

--- my_fastapi_app/app/util/youtube_api.py
+++ my_fastapi_app/app/util/youtube_api.py
@@ -1,13 +1,15 @@
 from googleapiclient.discovery import build
 from app.config import config
 from functools import lru_cache
+import json
 
+@lru_cache
 def yt():
     api_key = config.YOUTUBE_API
     return build('youtube', 'v3', developerKey=api_key)
 
 def search_videos_by_keyword(keyword, max_results=10, max_pages=5):
-    youtube = yt()  # Make sure this function returns a configured YouTube client
+    youtube = yt()  # Retrieve the configured YouTube client
     next_token = None
     page_count = 0
 
@@ -22,14 +24,15 @@ def search_videos_by_keyword(keyword, max_results=10, max_pages=5):
         response = request.execute()
 
         for item in response['items']:
-            video_id = item['id']['videoId']
-            title = item['snippet']['title']
-            description = item['snippet']['description']
-            print(response)
-            # Ensure you're yielding a properly formatted JSON string
-            yield f"data: {{\"video_id\": \"{video_id}\", \"title\": \"{title.replace('"', '\\"')}\", \"description\":\"{description.replace('"', '\\"')}\"}}\n\n"
+            video_info = {
+                "video_id": item['id']['videoId'],
+                "title": item['snippet']['title'],
+                "description": item['snippet']['description']
+            }
+            # Yield a properly formatted JSON string using json.dumps
+            yield f"data: {json.dumps(video_info)}\n\n"
 
         next_token = response.get('nextPageToken')
         if not next_token:
-            break
+            break  # Exit the loop if there's no more pages
         page_count += 1
